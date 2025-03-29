a60d26abbadb25042299c7d175eb48dac268a2d6
Filip2k03 <stephanfilip7@gmail.com>
2025-03-29 09:56:33 +0630

    deploy env pip install

--- student_management/settings.py
+++ student_management/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -123,3 +124,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+env = environ.Env()
+environ.Env.read_env()
+
+DATABASES = {
+    'default': env.db(),
+}
